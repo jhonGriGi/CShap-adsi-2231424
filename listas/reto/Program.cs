d54aaa3140b806bc5bc74a07cfc635df8ec1d529
jhonGriGi <darwingrisalesg@gmail.com>
2021-08-04 16:22:06 -0500

    saving changes

--- listas/reto/Program.cs
+++ listas/reto/Program.cs
@@ -7,7 +7,7 @@ namespace reto
     {
         static void Main(string[] args)
         {
-            // Declaracion de lista de estudiantes
+            //Declaracion de lista de estudiantes
             ArrayList estudiantes = new ArrayList();
 
             estudiantes.Add("ALEJANDRO CARDONA LOPEZ");
@@ -35,19 +35,7 @@ namespace reto
             estudiantes.Add("SIMON TORO CUERVO");
             estudiantes.Add("WESNER DAMIAN GARCES LORA");
             estudiantes.Add("YESICA ANDREA GUTIERREZ ESCOBAR");
-            
-            // Generando variables y array para asignar grupos
-            int length = estudiantes.Count, numero = 0;
-            int[] numeroAlAzar = new int[length];
 
-            // Generador de un numero al azar y guardarlo en un array
-            for(int j = 0; j < length; j++) {
-                Random rnd = new Random();
-                numero = rnd.Next(1, 8);
-                numeroAlAzar[j] = numero;
-            }
-            
-            // creando las lista de los grupos
             ArrayList tema1 = new ArrayList();
             ArrayList tema2 = new ArrayList();
             ArrayList tema3 = new ArrayList();
@@ -56,75 +44,126 @@ namespace reto
             ArrayList tema6 = new ArrayList();
             ArrayList tema7 = new ArrayList();
 
-            // Asignando los grupos
-            int i = 0;
-            foreach(string estudiante in estudiantes) {
-                switch (numeroAlAzar[i]) {
-                    case 1:
-                        tema1.Add(estudiante);
-                        break;
-                    case 2:
-                        tema2.Add(estudiante);
-                        break;
-                    case 3:
-                        tema3.Add(estudiante);
-                        break;
-                    case 4:
-                        tema4.Add(estudiante);
-                        break;
-                    case 5:
-                        tema5.Add(estudiante);
-                        break;
-                    case 6:
-                        tema6.Add(estudiante);
-                        break;
-                    case 7:
-                        tema7.Add(estudiante);
-                        break;
-                }
-                i++;
+            Random rnd = new Random();
+            int numero = 0;
+
+            foreach(string persona in estudiantes) {
+                bool asignacion = false;
+                do {
+                    numero = rnd.Next(1, 8);
+                    if(numero == 1) {
+                        if(tema1.Count < 3) {
+                            tema1.Add(persona);
+                            asignacion = true;
+                        }
+                        else {
+                            asignacion = false;
+                        }
+                    }
+                    else if(numero == 2) {
+                        if(tema2.Count < 3) {
+                            tema2.Add(persona);
+                            asignacion = true;
+                        }
+                        else {
+                            asignacion = false;
+                        }
+                    }
+                    else if(numero == 3) {
+                        if(tema3.Count < 3) {
+                            tema3.Add(persona);
+                            asignacion = true;
+                        }
+                        else {
+                            asignacion = false;
+                        }
+                    }
+                    else if(numero == 4) {
+                        if(tema4.Count < 4) {
+                            tema4.Add(persona);
+                            asignacion = true;
+                        }
+                        else {
+                            asignacion = false;
+                        }
+                    }
+                    else if(numero == 5) {
+                        if(tema5.Count < 4) {
+                            tema5.Add(persona);
+                            asignacion = true;
+                        }
+                        else {
+                            asignacion = false;
+                        }
+                    }
+                    else if(numero == 6) {
+                        if(tema6.Count < 4) {
+                            tema6.Add(persona);
+                            asignacion = true;
+                        }
+                        else {
+                            asignacion = false;
+                        }
+                    }
+                    else if(numero == 7) {
+                        if(tema7.Count < 4) {
+                            tema7.Add(persona);
+                            asignacion = true;
+                        }
+                        else {
+                            asignacion = false;
+                        }
+                    }
+                }while(asignacion == false);
             }
 
             Console.WriteLine("Los grupos resultantes son: ");
             Console.WriteLine("************************************\n");
             Console.WriteLine("Que es la Programación Orientada a Objetos POO");
-            foreach(string estudiante in tema1) {
+            foreach (string estudiante in tema1)
+            {
                 Console.WriteLine($"integrante: {estudiante}");
             }
             //
             Console.WriteLine("************************************\n");
             Console.WriteLine("Diferencias entre la programación estructurada y la Programación");
-            foreach(string estudiante in tema2) {
+            foreach (string estudiante in tema2)
+            {
                 Console.WriteLine($"integrante: {estudiante}");
             }
             //
             Console.WriteLine("************************************\n");
             Console.WriteLine("Abstracción en POO");
-            foreach(string estudiante in tema3) {
+            foreach (string estudiante in tema3)
+            {
                 Console.WriteLine($"integrante: {estudiante}");
             }
             //
             Console.WriteLine("************************************\n");
             Console.WriteLine("Encapsulación en POO");
-            foreach(string estudiante in tema4) {
+            foreach (string estudiante in tema4)
+            {
                 Console.WriteLine($"integrante: {estudiante}");
             }
             //
             Console.WriteLine("************************************\n");
             Console.WriteLine("Herencia en POO");
-            foreach(string estudiante in tema5) {
+            foreach (string estudiante in tema5)
+            {
                 Console.WriteLine($"integrante: {estudiante}");
             }
             //
             Console.WriteLine("************************************\n");
             Console.WriteLine("Polimorfismo en POO");
-            foreach(string estudiante in tema6) {
+            foreach (string estudiante in tema6)
+            {
                 Console.WriteLine($"integrante: {estudiante}");
             }
             //
             Console.WriteLine("************************************\n");
             Console.WriteLine("Que es un diagrama de Clases");
-            foreach(string estudiante in tema7) {
+            foreach (string estudiante in tema7)
+            {
                 Console.WriteLine($"integrante: {estudiante}");
             }
         }
